9d21cc73f311fdb98bf85de60c96ff1b9b363471
AhmadAkhtar32 <ahmadrao3226@gmail.com>
2025-03-12 22:32:13 +0500

    Modifing files

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import Homepage from './routes/homepage/Homepage.jsx';
 import RootLayout from './layouts/rootLayout/RootLayout.jsx';
+import DashboardLayout from './layouts/dashboardLayout/DashboardLayout.jsx';
+import Dashboardpage from './routes/dashboardpage/Dashboardpage.jsx';
+import Chatpage from './routes/chatpage/Chatpage.jsx';
 
 
 const router = createBrowserRouter([
@@ -14,6 +17,19 @@ const router = createBrowserRouter([
         path: '/',
         element: <Homepage />,
       },
+      {
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: '/dashboard',
+            element: <Dashboardpage />
+          },
+          {
+            path: '/dashboard/chats/:id',
+            element: <Chatpage />
+          }
+        ]
+      }
     ],
   },
 ]);
