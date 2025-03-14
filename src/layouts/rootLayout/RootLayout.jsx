303e632a6e6e78622f9b642b70865ec26b17ddef
AhmadAkhtar32 <ahmadrao3226@gmail.com>
2025-03-14 08:32:38 +0500

    adding clerk

--- src/layouts/rootLayout/RootLayout.jsx
+++ src/layouts/rootLayout/RootLayout.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import './Rootlayout.css';
+import { ClerkProvider } from '@clerk/clerk-react';
+
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+    throw new Error("Missing Publishable Key")
+}
 
 const RootLayout = () => {
     return (
-        <div className='rootLayout'>
-            <header>
-                <Link to="/" className='logo'>
-                    <img src="/logo.png" alt="NeuralTalk Logo" />
-                    <span>NeuralTalk</span>
-                </Link>
-                <div className="user">User</div>
-            </header>
-            <main>
-                <Outlet />
-            </main>
-        </div>
+
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+
+            <div className='rootLayout'>
+                <header>
+                    <Link to="/" className='logo'>
+                        <img src="/logo.png" alt="NeuralTalk Logo" />
+                        <span>NeuralTalk</span>
+                    </Link>
+                    <div className="user">User</div>
+                </header>
+                <main>
+                    <Outlet />
+                </main>
+            </div>
+        </ClerkProvider>
     );
 };
 
