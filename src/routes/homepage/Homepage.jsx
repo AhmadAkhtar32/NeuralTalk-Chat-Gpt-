b81156d313131c072738375120e42dc4e87b93dd
AhmadAkhtar32 <ahmadrao3226@gmail.com>
2025-03-15 22:55:24 +0500

    Adding text

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Homepage.css'
 import { Link } from 'react-router-dom'
+import { TypeAnimation } from 'react-type-animation'
 
 const Homepage = () => {
     return (
@@ -20,6 +21,26 @@ const Homepage = () => {
                         </div>
                     </div>
                     <img src="/bot.png" alt="img not found" className='bot' />
+                    <div className="chat">
+                        <img src="/bot.png" alt="" />
+                        <TypeAnimation
+                            sequence={[
+                                // Same substring at the start will only be typed out once, initially
+                                'We produce food for Mice',
+                                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                                'We produce food for Hamsters',
+                                1000,
+                                'We produce food for Guinea Pigs',
+                                1000,
+                                'We produce food for Chinchillas',
+                                1000
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            style={{ fontSize: '2em', display: 'inline-block' }}
+                            repeat={Infinity}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
