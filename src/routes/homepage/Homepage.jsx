b5e2b04181f7287feca0cd7fc3b306d0d4cd278e
AhmadAkhtar32 <ahmadrao3226@gmail.com>
2025-03-15 22:16:05 +0500

    installing react type library

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -5,7 +5,23 @@ import { Link } from 'react-router-dom'
 const Homepage = () => {
     return (
         <div className='homepage'>
-            <Link to="/dashboard" >Dashboard</Link>
+            <img src="/orbital.png" alt="img not found" className='orbital' />
+            <div className="left">
+                <h1>NeuralTalks</h1>
+                <h2>Seamless Chat. Limitless Possibilities.</h2>
+                <h3>Your personal AI companion, ready to chat anytime. NeuralTalk listens, learns, and responds with intelligence.</h3>
+                <Link to="/dashboard">Get Started</Link>
+            </div>
+            <div className="right">
+                <div className="imgContainer">
+                    <div className="bgContainer">
+                        <div className="bg">
+
+                        </div>
+                    </div>
+                    <img src="/bot.png" alt="img not found" className='bot' />
+                </div>
+            </div>
         </div>
     )
 }
