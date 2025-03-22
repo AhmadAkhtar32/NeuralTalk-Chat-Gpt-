f9473d7e052b833ff69ad10eb64fa3e149c018ab
AhmadAkhtar32 <ahmadrao3226@gmail.com>
2025-03-22 15:24:43 +0500

    updating chatlist

--- src/components/chatList/ChatList.jsx
+++ src/components/chatList/ChatList.jsx
@@ -1,38 +1,45 @@
-/* ChatList file */
-import React from 'react'
+import { Link } from "react-router-dom";
 import "./chatList.css";
-import { Link } from 'react-router-dom';
-
+import { useQuery } from "@tanstack/react-query";
 
 const ChatList = () => {
-    return (
-        <div>
-            <span className='title '>Dashboard</span>
+    const { isPending, error, data } = useQuery({
+        queryKey: ["userChats"],
+        queryFn: () =>
+            fetch(`${import.meta.env.VITE_API_URL}/api/userchats`, {
+                credentials: "include",
+            }).then((res) => res.json()),
+    });
 
-            <Link to="/dashboard">Create A New Chat</Link>
-            <Link to="/">Explore NeuralTalk</Link>
+    return (
+        <div className="chatList">
+            <span className="title">DASHBOARD</span>
+            <Link to="/dashboard">Create a new Chat</Link>
+            <Link to="/">Explore Lama AI</Link>
             <Link to="/">Contact</Link>
             <hr />
-            <span className='title '>RECENT CHATS</span>
+            <span className="title">RECENT CHATS</span>
             <div className="list">
-                <Link to="/">My Chat Title</Link>
-                <Link to="/">My Chat Title</Link>
-                <Link to="/">My Chat Title</Link>
-                <Link to="/">My Chat Title</Link>
-                <Link to="/">My Chat Title</Link>
-                <Link to="/">My Chat Title</Link>
+                {isPending
+                    ? "Loading..."
+                    : error
+                        ? "Something went wrong!"
+                        : data?.map((chat) => (
+                            <Link to={`/dashboard/chats/${chat._id}`} key={chat._id}>
+                                {chat.title}
+                            </Link>
+                        ))}
             </div>
             <hr />
             <div className="upgrade">
-                <img src="/logo.png" alt="img not found" />
+                <img src="/logo.png" alt="" />
                 <div className="texts">
-                    <span>Upgrade to Pro</span>
-                    <span>Unlock all features</span>
+                    <span>Upgrade to Lama AI Pro</span>
+                    <span>Get unlimited access to all features</span>
                 </div>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default ChatList
+export default ChatList;
